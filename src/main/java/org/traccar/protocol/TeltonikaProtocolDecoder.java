59bfd0fa3c340f0b2d2512b9cae268486b4789d5
ispdomnet <domnet.service.rozdil@gmail.com>
2025-12-19 10:40:05 +0200

    10

--- src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
@@ -259,6 +259,8 @@ public class TeltonikaProtocolDecoder extends BaseProtocolDecoder {
         register(88, fmbXXX, (p, b) -> p.set(Position.KEY_RPM, b.readUnsignedShort()));
         register(104, fmbXXX, (p, b) -> p.set(Position.KEY_HOURS, b.readUnsignedInt() * 3600000));
         register(113, fmbXXX, (p, b) -> p.set(Position.KEY_ODOMETER_SERVICE, b.readInt() * 1000));
+        register(123, fmb6XX, (p, b) -> p.set("tachoPerformance", b.readUnsignedByte()));
+        register(127, fmb6XX, (p, b) -> p.set("engineCT", b.readByte() - 40));
         register(128, fmb6XX, (p, b) -> p.set("ambientTemp", b.readShort()));
         register(135, fmbXXX, (p, b) -> p.set(Position.KEY_FUEL_CONSUMPTION, b.readUnsignedShort()));
         register(139, fmb6XX, (p, b) -> p.set("grossCombVWeight", b.readUnsignedInt()));
