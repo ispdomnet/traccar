751f965cc93638c063e606ce5d274c40e83f7af7
ispdomnet <domnet.service.rozdil@gmail.com>
2025-12-23 13:13:08 +0200

    11

--- src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
@@ -248,6 +248,7 @@ public class TeltonikaProtocolDecoder extends BaseProtocolDecoder {
         register(66, any, (p, b) -> p.set(Position.KEY_POWER, b.readUnsignedShort() * 0.001));
         register(67, any, (p, b) -> p.set(Position.KEY_BATTERY, b.readUnsignedShort() * 0.001));
         register(69, fmb6XX, (p, b) -> p.set("d1CmDT", b.readUnsignedShort()));
+        register(71, fmb6XX, (p, b) -> p.set("gnssStatus", b.readUnsignedByte()));
         register(78, fmb6XX, (p, b) -> {
             long driverUniqueId = b.readLongLE();
             if (driverUniqueId != 0) {
